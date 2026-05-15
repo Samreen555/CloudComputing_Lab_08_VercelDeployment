41e04ea2510fae778d703676993a01f46a786375
Samreen Farhat <farhatsamreen8@gmail.com>
2026-05-15 11:01:52 +0500

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Lab 8 – Vercel Deployment</h1>
+      <p>Name: [SAMREEN FARHAT]</p>
+      <p>Roll: [01-131232-079]</p>
+    </main>
+  );
+}
